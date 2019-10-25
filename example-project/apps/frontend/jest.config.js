bad42e2793d3efe2c43dbe5d5ee3ccdb02dbcbce
Yanick <yanick.dickbauer@cnc.io>
2019-10-25 11:20:44 +0200

    Deleted nx workspace

--- example-project/apps/frontend/jest.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  name: 'frontend',
-  preset: '../../jest.config.js',
-  coverageDirectory: '../../coverage/apps/frontend',
-  snapshotSerializers: [
-    'jest-preset-angular/AngularSnapshotSerializer.js',
-    'jest-preset-angular/HTMLCommentSerializer.js'
-  ]
-};
