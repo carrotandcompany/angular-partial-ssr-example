09a06b69b6f426b4f89e57bba319276aae69678c
Yanick <yanick.dickbauer@cnc.io>
2019-10-27 21:20:36 +0100

    Added a public page and set up docker files

--- frontend/src/app/public-page/public-page.component.ts
+++ frontend/src/app/public-page/public-page.component.ts
@@ -1,15 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Inject, OnInit, PLATFORM_ID } from "@angular/core";
+import { isPlatformBrowser } from "@angular/common";
 
 @Component({
-  selector: 'app-public-page',
-  templateUrl: './public-page.component.html',
-  styleUrls: ['./public-page.component.sass']
+  selector: "app-public-page",
+  template: "<p>Rendered by {{ renderer }}</p>"
 })
 export class PublicPageComponent implements OnInit {
+  renderer: string;
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(@Inject(PLATFORM_ID) platformId: any) {
+    this.renderer = isPlatformBrowser(platformId) ? "Browser" : "Server";
   }
 
+  ngOnInit() {}
 }
